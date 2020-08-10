3dd00790dbea86e06216dea4464b1af99abef0c1
Ardo <ardok@users.noreply.github.com>
2020-08-10 16:37:39 -0700

    [chore] show prompt if not https (#8)

--- src/components/containers/AppContainer.js
+++ src/components/containers/AppContainer.js
@@ -1,6 +1,25 @@
 // @flow
-import React from 'react';
+import React, { useEffect } from 'react';
+import window from 'global/window';
 import { styled } from 'styletron-react';
+
+import { isDev } from '../../utils/env';
+
+const useHttpsPrompt = () => {
+  useEffect(() => {
+    if (!isDev()) {
+      // Production
+      const { protocol } = window.location;
+      if (!protocol.includes('https')) {
+        // No https, prompt user
+        const isYes = window.confirm('Would you like to redirect to https?');
+        if (isYes) {
+          window.location.href = `https${window.location.href.slice(4)}`;
+        }
+      }
+    }
+  }, []);
+};
 
 const GAText = styled('div', {
   position: 'absolute',
@@ -9,11 +28,15 @@ const GAText = styled('div', {
   fontSize: '9px',
 });
 
-const AppContainer = ({ children }: { children: React$Node }) => (
-  <>
-    <div>{children}</div>
-    <GAText>This website is using Google Analytics</GAText>
-  </>
-);
+const AppContainer = (props: { children: React$Node }) => {
+  const { children } = props;
+  useHttpsPrompt();
+  return (
+    <>
+      <div>{children}</div>
+      <GAText>This website is using Google Analytics</GAText>
+    </>
+  );
+};
 
 export { AppContainer };
