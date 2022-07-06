de9877f8944573f9bc9afbad6d6883138ee2dc52
Ardo Kusuma <ardo@uber.com>
2022-07-05 22:19:05 -0700

    Try fixing extra /jkusuma-com

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  // basePath: process.env.NEXT_PUBLIC_BASE_PATH,
   // assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
   // https://stackoverflow.com/questions/58784444/nextjs-export-is-broken-no-css-no-js
   // https://nextjs.org/docs/api-reference/next.config.js/cdn-support-with-asset-prefix
@@ -9,6 +9,6 @@ const nextConfig = {
   // eslint: {
   //   dirs: ['src/**'],
   // },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
