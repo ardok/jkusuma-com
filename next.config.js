a9a6521f0ec24e8bb75e11f76eb15a33635b3d07
Ardo <ardok@users.noreply.github.com>
2022-07-05 20:21:21 -0700

    [fix] assetPrefix (#36)

--- next.config.js
+++ next.config.js
@@ -2,7 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH
+  // assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+  // https://stackoverflow.com/questions/58784444/nextjs-export-is-broken-no-css-no-js
+  // https://nextjs.org/docs/api-reference/next.config.js/cdn-support-with-asset-prefix
+  assetPrefix: './',
   // eslint: {
   //   dirs: ['src/**'],
   // },
