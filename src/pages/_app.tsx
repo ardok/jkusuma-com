abf0fb198a3872a9799a94c38584fdd869cb2322
Ardo <ardok@users.noreply.github.com>
2022-07-05 22:15:59 -0700

    Add icon, fix title, fix flash of unstyled content (#39)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import type { AppProps } from 'next/app';
 import { Provider as StyletronProvider } from 'styletron-react';
 import Script from 'next/script';
+import Head from 'next/head';
 
 import { styletron } from '../common/utils/styletron';
 import { useStyletron } from '../common/utils/styletron';
@@ -30,6 +31,9 @@ function MyApp({ Component, pageProps }: AppProps) {
       Putting the script here because otherwise nextjs will add some extra property into it somehow.
       https://github.com/vercel/next.js/discussions/22388#discussioncomment-2626860
       */}
+      <Head>
+        <title>Ardo Kusuma</title>
+      </Head>
       <Script src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js" />
       <StyletronProvider value={styletron}>
         <ComponentWrapper>
